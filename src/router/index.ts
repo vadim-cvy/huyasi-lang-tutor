8f5da23e5f3bdffb794574ccdd60ffc990e2c210
Vadim Cherepenichev <vadim.cvy@gmail.com>
2025-04-17 21:34:43 +0200

    Add "Dictionary", "Add Dictionary Item" and "Practice" pages (empty)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,24 @@
+import PracticeView from '@/views/PracticeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
+    {
+      path: '/',
+      name: 'practice',
+      component: PracticeView,
+    },
+    {
+      path: '/add-dictionary-item',
+      name: 'add-dictionary-item',
+      component: () => import('../views/AddDictionaryItemView.vue'),
+    },
+    {
+      path: '/dictionary',
+      name: 'dictionary',
+      component: () => import('../views/DictionaryView.vue'),
+    }
   ],
 })
 
