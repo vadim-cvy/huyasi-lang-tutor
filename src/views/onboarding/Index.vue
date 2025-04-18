89d9464abd5b2ec47b7590192f32428e7b8a744f
Egaodayo <japandaidaisukidesu@gmail.com>
2025-04-18 20:44:20 +0300

    added btn that do route /dic

--- src/views/onboarding/Index.vue
+++ src/views/onboarding/Index.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import Screen from '@/global-components/Screen.vue';
 import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
 
-const nativeLanguage: string[] = ['English', 'Russian', '日本語'];
-const educationLanguage: string[] = ['English', 'Russian', '日本語'];
-let choicedNativeLanguage = ref<string>('');
-let choicedEducationLanguage = ref<string>('');
+const router = useRouter();
+const languages: string[] = ['English', 'Russian', '日本語'];
+let languagesNativeLanguage = ref<string>('');
+let languagesEducationLanguage = ref<string>('');
 
-watch(choicedNativeLanguage, (newLang) => {
+watch(languagesNativeLanguage, (newLang) => {
   localStorage.setItem('userNaviteLanguage', newLang)
 })
 
@@ -15,13 +16,14 @@ watch(choicedNativeLanguage, (newLang) => {
 
 <template>
   <Screen>
-    <select v-model="choicedNativeLanguage" name="nativeLanguage" id="nativeLanguage">
+    <select v-model="languagesNativeLanguage" name="languages" id="languages">
       <option value="">Please, choice your language</option>
-      <option v-for="lang in nativeLanguage" :key="lang" :value="lang">{{ lang }}</option>
+      <option v-for="lang in languages" :key="lang" :value="lang">{{ lang }}</option>
     </select>
-    <select v-model="choicedEducationLanguage" name="educationLanguage" id="educationLanguage">
+    <select v-model="languagesEducationLanguage" name="languages" id="languages">
       <option value="">Please, choice your education language</option>
-      <option v-for="lang in educationLanguage" :key="lang" :value="lang">{{ lang }}</option>
+      <option v-for="lang in languages" :key="lang" :value="lang">{{ lang }}</option>
     </select>
+    <button @click="router.push('/dictionary')">Go on</button>
   </Screen>
 </template>
