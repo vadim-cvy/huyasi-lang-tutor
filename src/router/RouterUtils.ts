79642801dd26f7532c95471d0e969d263063925d
Vadim Cherepenichev <vadim.cvy@gmail.com>
2025-04-18 18:25:18 +0200

    Implement router utils to implement strict typed  wrapper for useRouter().push

---
+++ src/router/RouterUtils.ts
@@ -0,0 +1,43 @@
+import PracticeView from '@/views/practice/Index.vue'
+import { useRouter, type RouteRecordRaw } from 'vue-router'
+
+export class RouterUtils
+{
+  public static readonly routes = [
+    {
+      name: 'practice',
+      path: '/',
+      component: PracticeView,
+    },
+    {
+      path: '/add-dictionary-item',
+      name: 'add-dictionary-item',
+      component: () => import('../views/add-dictionary-item/Index.vue')
+    },
+    {
+      path: '/dictionary',
+      name: 'dictionary',
+      component: () => import('../views/dictionary/Index.vue')
+    },
+    {
+      path: '/onboarding',
+      name: 'onboarding',
+      component: () => import('../views/onboarding/Index.vue')
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import('../views/settings/Index.vue')
+    },
+    {
+      path: '/onborading',
+      name: 'onboarding',
+      component: () => import('../views/onboarding/Index.vue')
+    }
+  ] as const
+
+  public static push( routeName: typeof this.routes[number]['name'] ) : void
+  {
+    useRouter().push( { name: routeName } )
+  }
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,9 @@
-import PracticeView from '@/views/practice/Index.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { RouterUtils } from './routerUtils'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'practice',
-      component: PracticeView,
-    },
-    {
-      path: '/add-dictionary-item',
-      name: 'add-dictionary-item',
-      component: () => import('../views/add-dictionary-item/Index.vue')
-    },
-    {
-      path: '/dictionary',
-      name: 'dictionary',
-      component: () => import('../views/dictionary/Index.vue')
-    },
-    {
-      path: '/onboarding',
-      name: 'onboarding',
-      component: () => import('../views/onboarding/Index.vue')
-    }
-  ],
+  routes: RouterUtils.routes,
 })
 
 export default router
