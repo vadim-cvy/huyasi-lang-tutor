6a91868f959f50ed085c0754911b39e6e2a875df
Vadim Cherepenichev <vadim.cvy@gmail.com>
2025-04-19 01:09:08 +0200

    Implement draft header

--- src/global-components/GlobalHeader.vue
+++ src/global-components/GlobalHeader.vue
@@ -1,4 +1,32 @@
+<script setup lang="ts">
+import Logo from './Logo.vue';
+
+defineProps<{
+  title: string
+}>()
+</script>
+
 <template>
-  // todo: logo (pencil)
-  // todo: current screen name
+  <div class="global-header">
+    <Logo/>
+
+    <h1 class="global-header__title">
+      {{ title }}
+    </h1>
+  </div>
 </template>
+
+<style lang="scss">
+.global-header
+{
+  display: flex;
+  gap: 20px;
+
+  &__title
+  {
+    width: 100%;
+    text-align: center;
+    font-size: 24px;
+  }
+}
+</style>
---
+++ src/global-components/Logo.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import logoSrc from '@/assets/img/logo.png'
+import { RouterUtils } from '@/router/RouterUtils';
+</script>
+
+<template>
+  <a class="logo" @click="RouterUtils.push( 'home' )">
+    <img class="logo__img" :src="logoSrc" alt="Logo">
+
+    <span class="logo__text">
+      Huyasi
+    </span>
+  </a>
+</template>
+
+<style lang="scss">
+@use '@/assets/css/mixins/typography.scss';
+.logo
+{
+  display: flex;
+  gap: 5px;
+  align-items: center;
+
+  &__img
+  {
+    height: 20px;
+  }
+
+  &__text
+  {
+    @include typography.h1;
+    font-size: 24px;
+  }
+}
+</style>
