26b6139290f26ec55d7d153ac2f0b5c30478667c
Vadim Cherepenichev <vadim.cvy@gmail.com>
2025-04-19 01:07:58 +0200

    Refactor

--- src/router/RouterUtils.ts
+++ src/router/RouterUtils.ts
@@ -3,41 +3,26 @@ import { useRouter, type RouteRecordRaw } from 'vue-router'
 
 export class RouterUtils
 {
-  public static readonly routes = [
-    {
-      name: 'practice',
-      path: '/',
-      component: PracticeView,
-    },
-    {
-      path: '/add-dictionary-item',
-      name: 'add-dictionary-item',
-      component: () => import('../views/add-dictionary-item/Index.vue')
-    },
-    {
-      path: '/dictionary',
-      name: 'dictionary',
-      component: () => import('../views/dictionary/Index.vue')
-    },
-    {
-      path: '/onboarding',
-      name: 'onboarding',
-      component: () => import('../views/onboarding/Index.vue')
-    },
-    {
-      path: '/settings',
-      name: 'settings',
-      component: () => import('../views/settings/Index.vue')
-    },
-    {
-      path: '/onboarding',
-      name: 'onboarding',
-      component: () => import('../views/onboarding/Index.vue')
-    }
+  private static readonly viewDirNames = [
+    'home',
+    'practice',
+    'add-dictionary-item',
+    'dictionary',
+    'onboarding',
+    'settings'
   ] as const
 
-  public static push( routeName: typeof this.routes[number]['name'] ) : void
+  public static get routes() : RouteRecordRaw[]
   {
-    useRouter().push( { name: routeName } )
+    return this.viewDirNames.map( viewDirName => ({
+      name: viewDirName,
+      path: '/' + ( viewDirName === 'home' ? '' : viewDirName ),
+      component: () => import( `../views/${viewDirName}/Index.vue` )
+    }))
+  }
+
+  public static push( viewDirName: typeof this.viewDirNames[number] )
+  {
+    useRouter().push( viewDirName )
   }
 }
