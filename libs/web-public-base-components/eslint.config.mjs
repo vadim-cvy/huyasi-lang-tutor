c6518d04d360227c69ac6cdb18e5b349bc160405
Vadim Cherepenichev <vadim.cerepenicev@Bragman>
2025-12-27 20:55:15 +0100

    Fix prefix for web-public-base-components lib
    Each lib should have its own prefix to avoid mess when project grows.
    
    Task: https://trello.com/c/qYKfehh3/2-implement-public-ui-app-basic-structure

--- libs/web-public-base-components/eslint.config.mjs
+++ libs/web-public-base-components/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'lib',
+          prefix: 'webPublicBaseComponents',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'lib',
+          prefix: 'web-public-base-components',
           style: 'kebab-case',
         },
       ],
