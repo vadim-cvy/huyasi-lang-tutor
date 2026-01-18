4b093f4e0284953022a0bcaf53be38f8f4dac4dd
Vadim Cherepenichev <vadim.cerepenicev@Bragman>
2026-01-18 14:00:47 +0100

    Import footer into web-public app and setup footer/sidebar visibility toggling on screen resize
    Task: https://trello.com/c/qYKfehh3/2-implement-public-ui-app-basic-structure

--- apps/web-public/src/app/app.ts
+++ apps/web-public/src/app/app.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { RouterModule } from '@angular/router';
-import { Header, Sidebar } from '@huyasi/web-public-base-components'
+import { Footer, Header, Sidebar } from '@huyasi/web-public-base-components'
+import { auditTime, fromEvent, map, startWith } from 'rxjs';
 
 @Component({
-  imports: [RouterModule, Header, Sidebar],
+  imports: [RouterModule, Header, Sidebar, Footer],
   selector: 'app-root',
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  private destroyRef = inject(DestroyRef);
+
+  public isSidebarVisible = signal<boolean>(false);
+  public isFooterVisible = signal<boolean>(false);
+
+  constructor() {
+    this.syncSidebarAndFooterVisibilityOnScreenWidthChange();
+  }
+
+  private syncSidebarAndFooterVisibilityOnScreenWidthChange(): void {
+    const throttleMs = 100;
+
+    const getScreenWidth = () => window.innerWidth;
+
+    const resize$ = fromEvent(window, 'resize').pipe(
+      auditTime(throttleMs),
+      map(getScreenWidth),
+      startWith(getScreenWidth()),
+      takeUntilDestroyed(this.destroyRef),
+    )
+
+    resize$.subscribe(screenWidth => {
+      this.isSidebarVisible.set(screenWidth >= 960);
+      this.isFooterVisible.set(!this.isSidebarVisible());
+    });
+  }
+}
