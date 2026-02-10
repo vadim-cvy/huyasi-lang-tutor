bcd7ef70c093f9ec6488a828bf5394f88e31124b
Vadim Cherepenichev <vadim.cerepenicev@Bragman>
2026-02-10 16:11:26 +0100

    refactor (libs/shared/web-client/base/ui-components): remove nav-primary component export
    Why: apps will use higher-lvl components (footer, sidebar) and I don't see any cases when the apps may need to use nav-primary component directly.
    
    Task: https://trello.com/c/qYKfehh3/2-implement-public-ui-app-basic-structure

--- libs/shared/web-client/base/ui-components/src/index.ts
+++ libs/shared/web-client/base/ui-components/src/index.ts
@@ -3,5 +3,3 @@ export * from './lib/page/page';
 export * from './lib/header/header';
 export * from './lib/sidebar/sidebar';
 export * from './lib/footer/footer';
-
-export * from './lib/nav-primary/nav-primary';
