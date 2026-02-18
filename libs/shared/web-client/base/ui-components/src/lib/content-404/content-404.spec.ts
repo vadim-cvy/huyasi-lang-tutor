34e7194080ffa3a90a771f51660716ed81b37afb
Vadim Cherepenichev <vadim.cerepenicev@Bragman>
2026-02-18 14:31:56 +0100

    fix (libs/shared/web-client/base/ui-components: Content404): fix test "no provider found for ActivatedRoute" error
    Task: https://trello.com/c/qYKfehh3/2-implement-public-ui-app-basic-structure

--- libs/shared/web-client/base/ui-components/src/lib/content-404/content-404.spec.ts
+++ libs/shared/web-client/base/ui-components/src/lib/content-404/content-404.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Content404 } from './content-404';
+import { provideRouter } from '@angular/router';
 
 describe('Content404', () => {
   let component: Content404;
@@ -7,6 +8,7 @@ describe('Content404', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [provideRouter([])],
       imports: [Content404],
     }).compileComponents();
 
