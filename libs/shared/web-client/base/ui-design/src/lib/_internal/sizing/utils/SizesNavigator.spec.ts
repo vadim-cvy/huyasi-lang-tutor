a9372164a966b60a821b88d0ea7c2230da1b6e6b
Vadim Cherepenichev <vadim.cerepenicev@Bragman>
2026-03-08 16:42:24 +0100

    tests (libs/shared/web-client/base/ui-design: internal/sizing): add missing tests for SizesNavigator
    Task: https://trello.com/c/qYKfehh3/2-implement-public-ui-app-basic-structure

--- libs/shared/web-client/base/ui-design/src/lib/_internal/sizing/utils/SizesNavigator.spec.ts
+++ libs/shared/web-client/base/ui-design/src/lib/_internal/sizing/utils/SizesNavigator.spec.ts
@@ -1,44 +1,61 @@
 import { sizesAll } from '../data/sizesAll';
 import { SizesNavigator } from './SizesNavigator';
 
-const sizesNavigator = new SizesNavigator(sizesAll);
+const sizesNavigatorFull = new SizesNavigator(sizesAll);
+const sizesNavigatorSubset = new SizesNavigator(['sm', 'md', 'lg', 'xl']);
 
 describe('SizesNavigator', () => {
-  // FIXME: add test for invalid sizes subset in constructor
+  describe('constructor', () => {
+    it('should fail when sizes array is empty', () => {
+      expect(() => new SizesNavigator([])).toThrowError();
+    });
 
-  // FIXME: add test which ensures that sizes which are not presented in subset won't be returned by getSize... methods
+    it('should fail when sizes are out of order', () => {
+      expect(() => new SizesNavigator(['lg', 'md'])).toThrowError();
+    });
+
+    it('should fail when there are duplicated sizes', () => {
+      expect(() => new SizesNavigator(['sm', 'md', 'md', 'lg'])).toThrowError();
+    });
+  });
 
   describe('getSizeGreaterOrMax', () => {
     it('returns a larger size by the given number of steps', () => {
       const stepsForward = 2;
-      expect(sizesNavigator.getSizeGreaterOrMax('sm', stepsForward)).toBe('lg');
+      expect(sizesNavigatorFull.getSizeGreaterOrMax('sm', stepsForward)).toBe('lg');
+      expect(sizesNavigatorSubset.getSizeGreaterOrMax('sm', stepsForward)).toBe('lg');
     });
 
     it('returns the same size when steps are zero', () => {
       const stepsForward = 0;
-      expect(sizesNavigator.getSizeGreaterOrMax('md', stepsForward)).toBe('md');
+      expect(sizesNavigatorFull.getSizeGreaterOrMax('md', stepsForward)).toBe('md');
+      expect(sizesNavigatorSubset.getSizeGreaterOrMax('md', stepsForward)).toBe('md');
     });
 
     it('returns max size when desired index is out of bounds', () => {
       const stepsForward = 10;
-      expect(sizesNavigator.getSizeGreaterOrMax('xl', stepsForward)).toBe('2xl');
+      expect(sizesNavigatorFull.getSizeGreaterOrMax('md', stepsForward)).toBe('2xl');
+      expect(sizesNavigatorSubset.getSizeGreaterOrMax('md', stepsForward)).toBe('xl');
     });
   });
 
   describe('getSizeLessOrMin', () => {
     it('returns a smaller size by the given number of steps', () => {
       const stepsBackward = 2;
-      expect(sizesNavigator.getSizeLessOrMin('xl', stepsBackward)).toBe('md');
+      expect(sizesNavigatorFull.getSizeLessOrMin('xl', stepsBackward)).toBe('md');
+      expect(sizesNavigatorSubset.getSizeLessOrMin('xl', stepsBackward)).toBe('md');
     });
 
     it('returns the same size when steps are zero', () => {
       const stepsBackward = 0;
-      expect(sizesNavigator.getSizeLessOrMin('md', stepsBackward)).toBe('md');
+      expect(sizesNavigatorFull.getSizeLessOrMin('md', stepsBackward)).toBe('md');
+      expect(sizesNavigatorSubset.getSizeLessOrMin('md', stepsBackward)).toBe('md');
     });
 
     it('returns min size when desired index is out of bounds', () => {
       const stepsBackward = 10;
-      expect(sizesNavigator.getSizeLessOrMin('sm', stepsBackward)).toBe('xs');
+      expect(sizesNavigatorFull.getSizeLessOrMin('md', stepsBackward)).toBe('xs');
+      expect(sizesNavigatorSubset.getSizeLessOrMin('md', stepsBackward)).toBe('sm');
     });
   });
 });
