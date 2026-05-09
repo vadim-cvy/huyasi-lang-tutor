7df83df648d80b151bc07a698504d337c159194a
Vadim Cherepenichev <vadim.cerepenicev@Bragman>
2026-05-09 15:12:31 +0200

    refactor (libs/core/auth-bff/openid-connect): remove extraScopes param from OpenidClientService.buildLoginUrl method.
    We don't really use extra scopes like profile and email right in the auth  service, so this param will probably never be used in this service and we don't need it.
    Profile and email can be obtained from a different auth0 endpoint, so we'll probably obtain that data in users service (which will be implemented in the future).
    
    Task: https://trello.com/c/9IB3gz3a/27-implement-auth-bff

--- libs/core/auth-bff/openid-connect/src/lib/openid-client/openid-client.service.ts
+++ libs/core/auth-bff/openid-connect/src/lib/openid-client/openid-client.service.ts
@@ -1,10 +1,8 @@
 import { AppType, AppTypeCurrentContextIdPayloadService } from '@huyasi/core-auth-bff-app-type';
 import { Injectable } from '@nestjs/common';
-import { randomBytes } from 'crypto';
 import * as openidClient from 'openid-client';
 
 import { OpenidConnectConfigService } from '../config/openid-connect-config.service';
-import { OpenidConnectScope } from './openid-connect-scope.type';
 
 // FIXME: AppTypeCurrentContextIdPayloadService makes this provider request-scoped too. But I need to check if it becomes durable request-scoped provider. And mark it as durable if it isn't.
 @Injectable()
@@ -30,20 +28,14 @@ export class OpenidClientService {
     private readonly openidConnectConfigService: OpenidConnectConfigService,
   ) {}
 
-  public async buildLoginUrl(
-    loginCallbackUrl: URL,
-    extraScopes?: Set<Exclude<OpenidConnectScope, 'oidc'>>,
-  ): Promise<{ url: URL; state: string }> {
+  public async buildLoginUrl(loginCallbackUrl: URL): Promise<{ url: URL; state: string }> {
     const config = await this.configPromise;
 
-    const scopes = new Set<OpenidConnectScope>(['oidc', ...(extraScopes ?? [])]);
-
-    const stateRandomBytesSize = 32;
-    const state = randomBytes(stateRandomBytesSize).toString('base64url');
+    const state = openidClient.randomState();
 
     const url = openidClient.buildAuthorizationUrl(config, {
       ['redirect_uri']: loginCallbackUrl.href,
-      scope: [...scopes].join(' '),
+      scope: 'openid',
       state,
     });
 
--- libs/core/auth-bff/openid-connect/src/lib/openid-client/openid-connect-scope.type.ts
+++
@@ -1,1 +0,0 @@
-export type OpenidConnectScope = 'oidc' | 'profile' | 'email';
